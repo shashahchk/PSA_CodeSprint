44830acf68aae00374a649de47e2856f31d1b3f7
shashah <shashaminchok@gmail.com>
2023-09-29 23:17:35 +0800

    add incoming orders page and backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, request
+import pandas as pd
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.config['SECRET_KEY'] = 'secret!'
 def index():
     return "Hello, World!"
 
+
 @app.route('/analyse', methods=['GET', 'POST'])
 @cross_origin(options=None)
 def analyse():
@@ -19,6 +21,13 @@ def analyse():
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     return "Post method only please!"
 
+@app.route('/api/incomingOrders', methods=['GET'])
+@cross_origin(options=None)
+def get_orders():
+    # Read the Excel file
+    df = pd.read_excel('dataset/orderdataset.xlsx', engine='openpyxl')
+    # Convert the DataFrame into JSON format and return
+    return jsonify(df.to_dict(orient='records'))
 
 if __name__ == '__main__':
     app.run(debug=True)

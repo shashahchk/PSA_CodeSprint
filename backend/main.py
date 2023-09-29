7f91896c44407815dcc54124b0dbe31b993a71e7
matochichap <ruijia.chan@gmail.com>
2023-09-29 23:27:18 +0800

    add xlsx_to_json

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
@@ -8,7 +9,13 @@ app.config['SECRET_KEY'] = 'secret!'
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    orders = xlsx_to_json('../dataset/Order Dataset.xlsx')
+    records = xlsx_to_json('../dataset/Ship Dataset.xlsx')
+    result = {
+        'orders': orders,
+        'records': records
+    }
+    return result
 
 @app.route('/analyse', methods=['GET', 'POST'])
 @cross_origin(options=None)
@@ -19,6 +26,10 @@ def analyse():
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     return "Post method only please!"
 
+def xlsx_to_json(excel_path):
+    df = pd.read_excel(excel_path)
+    result = df.to_json(orient='records')
+    return json.loads(result)
 
 if __name__ == '__main__':
     app.run(debug=True)

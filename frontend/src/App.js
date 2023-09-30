8187e8da377e5e463afabc1b994d2c6141eeeab4
jonasongg <jonasong2@gmail.com>
2023-09-30 14:36:29 +0800

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import IncomingOrders from './components/IncomingOrders';
-import LoginPage from './LoginPage';
+import logo from "./logo.svg";
+import "./App.css";
+import IncomingOrders from "./components/IncomingOrders";
+import LoginPage from "./LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <IncomingOrders />
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/incoming_orders" element={<IncomingOrders />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

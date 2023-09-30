fca5bc19a40c863c87b58ea1f0dcd28f5bc6c51b
matochichap <ruijia.chan@gmail.com>
2023-09-30 17:06:34 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,18 +7,17 @@ app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 
-@app.route('/api/fetch-data')
-def index():
-    orders = xlsx_to_json('../dataset/Order Dataset.xlsx')
-    ships = xlsx_to_json('../dataset/Ship Dataset.xlsx')
-    result = {
-        'orders': orders,
-        'ships': ships
-    }
-    return result
+@app.route('/api/orders')
+def orders():
+    orders = xlsx_to_json('../dataset/order_dataset.xlsx')
+    return seperate_incoming_outgoing(orders)
 
+@app.route('/api/ships')
+def ships():
+    ships = xlsx_to_json('../dataset/ship_dataset.xlsx')
+    return seperate_incoming_outgoing(ships)
 
-@app.route('/api/analyse', methods=['GET', 'POST'])
+@app.route('/api/assign_orders', methods=['GET', 'POST'])
 @cross_origin(options=None)
 def analyse():
     if request.method == 'POST':
@@ -31,6 +30,28 @@ def xlsx_to_json(excel_path):
     df = pd.read_excel(excel_path)
     result = df.to_json(orient='records')
     return json.loads(result)
+
+def seperate_incoming_outgoing(entries):
+    INTERMEDIATE_PORT = 'Singapore'
+    incoming = []
+    outgoing = []
+
+    for entry in entries:
+        if entry['Port of Destination'] == INTERMEDIATE_PORT:
+            incoming.append(entry)
+            continue
+        if entry['Port of Origin'] == INTERMEDIATE_PORT:
+            outgoing.append(entry)
+            continue
+        print(f'{INTERMEDIATE_PORT} not a destination or origin')
+
+    result = {
+        'incoming': incoming,
+        'outgoing': outgoing
+    }
+
+    return result
+
 
 if __name__ == '__main__':
     app.run(debug=True)
